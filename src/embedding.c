89022ca635e7a49a1455824968d8a5c01c3fa3a5
armgong <armgong@yahoo.com>
2014-06-28 08:55:50 +0800

    change julia_init logical make it can parrelle start n julia process,but nowthis need path julia client.jl so it only experimental

--- src/embedding.c
+++ src/embedding.c
@@ -12,6 +12,37 @@ Copyright (C) 2014 by Yu Gong
 #include "R_Julia.h"
 
 #define pkgdebug
+#define JL_SYSTEM_IMAGE_PATH "../lib/julia/sys.ji"
+extern DLLEXPORT char *julia_home;
+char *image_file = NULL;
+char system_image[256] = JL_SYSTEM_IMAGE_PATH;
+
+int true_main(int argc, char *argv[])
+{
+    if (jl_base_module != NULL) {
+        jl_array_t *args = (jl_array_t*)jl_get_global(jl_base_module, jl_symbol("ARGS"));
+        if (args == NULL) {
+            args = jl_alloc_cell_1d(0);
+            jl_set_const(jl_base_module, jl_symbol("ARGS"), (jl_value_t*)args);
+        }
+        jl_array_grow_end(args, argc);
+        int i;
+        for (i=0; i < argc; i++) {
+            jl_value_t *s = (jl_value_t*)jl_cstr_to_string(argv[i]);
+            s->type = (jl_value_t*)jl_utf8_string_type;
+           // Rprintf("%d %s\n",i,argv[i]);
+            jl_arrayset(args, s, i);
+       }
+    }
+    jl_function_t *start_client =
+        (jl_function_t*)jl_get_global(jl_base_module, jl_symbol("_startnoterm"));
+
+    if (start_client) {
+        jl_apply(start_client, NULL, 0);
+    }
+    return 0;
+}
+
 static int jlrunning=0;
 
 static int DataArrayFrameInited=0;
@@ -47,23 +78,39 @@ SEXP Julia_is_running()
   return ans; 
 }
 
-SEXP initJulia(SEXP julia_home,SEXP DisableGC)
+SEXP initJulia(SEXP julia_home1,SEXP DisableGC)
 {
   if (jl_is_initialized())
     return R_NilValue;
-  const char *s = CHAR(STRING_ELT(julia_home, 0));
-  if (strlen((char*)s) == 0)
-    jl_init(NULL);
-  else
-    jl_init((char*)s);
-  JL_SET_STACK_BASE;
+  julia_home =(char*) CHAR(STRING_ELT(julia_home1, 0));
+  int argc1=0;
+  char **argv1=NULL;
+
+  /*char worker[10];
+  snprintf(worker,10,"%d",INTEGER(Workers)[0]);
+  argv1[1]=worker;*/
+
+  char realimage[1024];
+  if (julia_home[strlen(julia_home)-1]=='/'
+    ||julia_home[strlen(julia_home)-1]=='\\')
+   snprintf(realimage,1024,"%s%s",julia_home,system_image); 
+  else  
+   snprintf(realimage,1024,"%s/%s",julia_home,system_image);
+  
+  libsupport_init();
+  julia_init(realimage);
+  julia_trampoline(argc1, argv1, true_main);
   jlrunning=1;
-  if (jl_exception_occurred())
-  {
-    error("Julia not initialized");
+
+  if (jl_exception_occurred()){
+    jlrunning=0;
+    jl_show(jl_stderr_obj(), jl_exception_occurred());
+    Rprintf("\n");
+    jl_exception_clear();
   }
   if (LOGICAL(DisableGC))
     jl_gc_disable();
+  Rprintf("Julia Init success\n");
   return R_NilValue;
 }
 
@@ -81,6 +128,7 @@ SEXP Julia_R(jl_value_t* Var)
    ans=Julia_R_MD(Var); 
   }
   else if (strcmp(jl_typeof_str(Var),"DataArray")==0||
+           strcmp(jl_typeof_str(Var),"PoolDataArray")==0||
            strcmp(jl_typeof_str(Var),"DataVector")==0||
            strcmp(jl_typeof_str(Var),"DataMatrix")==0||
            strcmp(jl_typeof_str(Var),"DataFrame")==0||
@@ -94,11 +142,16 @@ SEXP Julia_R(jl_value_t* Var)
      return R_NilValue;
     }
     if(strcmp(jl_typeof_str(Var),"NAtype")==0||
-       strcmp(jl_typeof_str(Var),"DataFrame")==0)
+       strcmp(jl_typeof_str(Var),"DataFrame")==0||
+       strcmp(jl_typeof_str(Var),"PoolDataArray")==0)
      { 
       if(strcmp(jl_typeof_str(Var),"NAtype")==0)
         ans=Julia_R_Scalar_NA(Var); 
-      else
+      else if (strcmp(jl_typeof_str(Var),"PoolDataArray")==0)
+       {
+        //ans=Julia_R_MD_NA_PoolArray(Var);
+       }
+      else 
         ans=Julia_R_MD_NA_DataFrame(Var);
      } 
     else 
